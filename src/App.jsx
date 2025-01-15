69f83a4b8060a988e9a97cbabe093ba06485f55d
AdwinTS <adwintsunil8@gmail.com>
2025-01-15 20:29:20 +0530

    some updations on personalized text

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import GetStarted from "./components/GetStarted";
  import Login from "./components/Login";
 import Signup from "./components/Signup";
 import TwoComponent from "./components/TwoComponent";
+import PersonalizedTest from "./components/personalizedtext";
 function App() {
   return (
     <Router>
@@ -17,7 +18,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/twocomponent" element={<TwoComponent />} />
-        
+        <Route path="/personalized-test" element={<PersonalizedTest />} />
 
       </Routes>
     </Router>
--- src/components/TwoComponent.jsx
+++ src/components/TwoComponent.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./TwoComponent.css";
+
 function TwoComponent() {
     return (
         <div className="vertical-tabs-container">
-          <div className="tab top-tab">
+          <div className="tab1 top-tab">
             <Link to="/personalized-test" className="tab-link">
               Personalized Test
             </Link>
           </div>
-          <div className="tab bottom-tab">
+          <div className="tab1 bottom-tab">
             <Link to="/other-feature" className="tab-link">
               Other Feature
             </Link>
